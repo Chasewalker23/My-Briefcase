cb0dd658ae2167bbe38b86a239510bec7d7d13a8
ChaseWalker <ldlasotta@hotmail.com>
2020-12-19 23:33:44 -0500

    More updates. Now Js

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Welcome to my Briefcase!
 
  This is my portfolio, where I will be updating my projects that are to come. This page took me a good amount of hours but i am happy with the final result. That being said, let me guide you through.
+
+ ## First Page!
+
+ Right here we have a look to what the first page will be
+
+ ![](assets/images/Capture1.png)
--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,10 @@
-var ready  = confirm("Oh, Hello there! Are you ready to check my briefcase? I just need your Username.");
+var ready  = confirm("Oh, Hello there! Welcome to my porftolio! Do you want to provide your name? Since.. You know mine");
 
-function confirmation() {
 if (ready)   {
     var username  = prompt("What it will be the name of the person that wants to see my portfolio?");
-    alert("That a cool name! " + username + " Well i hope you like my portfolio");
+    alert("That a cool name! " + username + " Well i hope you like my portfolio. Enjoy!");
 } 
-else    {
-    alert("I'm sorry but in order to see my awesome page. I need to trust you!")
-    confirmation();
+else     {
+    alert("Mmmm that's a shame, i wanted to meet you. Enjoy!")
+    
 }
-}
-
